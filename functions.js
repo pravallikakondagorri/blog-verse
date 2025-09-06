51370db881c8c1fae020bced0e1969f69f00c0e5
pravallikakondagorri <pravallikakondagorri644@gmail.com>
2025-09-06 15:54:10 +0530

    function

---
+++ functions.js
@@ -0,0 +1,32 @@
+function greet()
+{
+    console.log("dev astra")
+}
+greet()
+
+
+function add(x,y)
+{
+    return x+y;
+}
+let result=add(20,35)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+
+const multiply= function(x,y)
+{
+    return x*y;
+}
+console.log(multiply(5,3))
+
+//arrow function
+const square =(x)=> x*x;
+console.log(square(5))
+const sub =(x,y)=>{
+    //console.log(x,y);
+    return x-y;
+}
+console.log(sub(20,15))
