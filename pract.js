49edbd0c2a27e06402a141b7655d70d9a3e5fd4f
pravallikakondagorri <pravallikakondagorri644@gmail.com>
2025-09-06 12:33:36 +0530

    system

--- pract.js
+++ pract.js
@@ -1,1 +1,33 @@
 console.log("hello world,pravallika")
+
+let name = "pravallika"
+console.log(name)
+let num = 18
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+
+let x
+let y=null
+console.log(y)
+
+let islogged = false
+console.log(islogged)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+
+
+
+let arr =[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person ={
+    name : "pravallika"
+    age : 18
+
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
